453097aeab190597d206b9448f67bdd2ba201b99
Nikita Ushakov <ushakovn@tcd.ie>
2018-11-20 22:45:06 +0000

    1.1
    Added getFollowers function

--- github.R
+++ github.R
@@ -17,3 +17,26 @@ token <- config(token = oauth2.0_token(oauth_endpoints("github"),
                               key = "6141c2c391718b210db8", 
                               secret = "d9248f312fbd73f3cbbf3f4cdb54807fbfc6118d")))
 
+# Returns a dataframe with information on the Current Users Followers 
+getFollowers <- function(username)
+{
+    i <- 1
+    x <- 1
+    followersDF <- data.frame()
+    
+    while(x != 0)
+    {
+        followers           <- GET(paste0(url_git, username, "/followers?per_page=100&page=", i), token)
+        followersContent    <- content(followers)
+        currentFollowersDF  <- lapply(followersContent, function(x) 
+        {
+            df <- data.frame(user = x$login, userID = x$id, followersURL = x$followers_url, followingURL = x$following_url)
+        }) %>% bind_rows()
+
+        i <- i + 1
+        x <- length(followersContent)
+        followersDF <- rbind(followersDF, currentFollowersDF)
+    }
+
+    return(followersDF)
+}
